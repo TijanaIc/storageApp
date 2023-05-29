fca32c31a80ca489d6890d58226a3c5e5cdfce56
Tijana Cvetkovic <tijana88iv@gmail.com>
2023-05-29 13:33:09 +0200

    Added Storage.Domain (classes name of table)

---
+++ Storage.Domain/Products.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Storage.Domain
+{
+    public class Products
+    {
+        public int ProductId { get; set; }
+        public string NameOfProduct { get; set; }
+        public double Cost { get; set; }
+    }
+}
---
+++ Storage.Domain/Storages.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Storage.Domain
+{
+    public class Storages
+    {
+        public int StorageId { get; set; }
+        public string NameOfStoraget { get; set; }
+        public string KindOfStorage { get; set; }
+    }
+}
