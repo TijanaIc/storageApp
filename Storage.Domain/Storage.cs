0941cca6f7acf48c874ecb5d4de8da91eb26bd78
Tijana Cvetkovic <tijana88iv@gmail.com>
2023-05-31 11:09:43 +0200

    rename Storages to Storage

---
+++ Storage.Domain/Storage.cs
@@ -0,0 +1,9 @@
+﻿namespace Storage.Domain
+{
+    public class Storage
+    {
+        public int StorageId { get; set; }
+        public string? NameOfStoraget { get; set; }
+        public string? KindOfStorage { get; set; }
+    }
+}
